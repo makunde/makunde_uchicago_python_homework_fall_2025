8872d4f639a0aae98efb84acbc8d26becdd6b1b9
George Martin <georgeg10live@gmail.com>
2025-11-18 13:03:08 -0600

    implement scrable word tool that takes in scores and outputs top 15 words

---
+++ assignment_6/problem_2.py
@@ -0,0 +1,100 @@
+"""Now that we are becoming better Scrabble players, construct a program that will help us identify the highest scoring
+words from our rack of tiles.
+
+Write a program that prompts the user to enter the tiles in their rack as below:
+
+> Enter the each letter in your rack:
+t r a b w q f
+Your program should validate the inputed data and prompt the user if they enter a bad character (eg. a number).
+
+Also, note that a rack contains at most seven tiles.
+
+You should be able to handle input entered in any of the following formats:
+
+trabwqf
+t r a b w q f
+t,r,a,b,w,q,f
+Use regular expression to test which input format the user has entered. You may use any approach to separate the characters.
+
+Once you have isolated the letters, identify every word combination of any length that can be made from the letters. Use the
+itertools. package to assist in identifying the combinations and permutations of the letters. Use
+the list of allowed Scrabble words, scrabble_list.txt, to verify if the letter combinations you have created represents a
+valid word.
+
+Present a list to the user of the 15 highest scoring words for each word length from the available letters. The output
+should be sorted in descending order. If there are no possible words of a given length, report "no words". Follow the
+example given below:
+
+> Enter the each letter in your rack:
+t r a b w q 3
+3 is not a valid letter. Please try again.
+
+
+> Enter the letters in your rack?
+trabwqe
+
+
+2 Letter Words
+--------------
+aa - 2 points
+no - 2 points
+...
+...
+
+
+3 Letter Words
+--------------
+bat - 6 points
+rat - 7 points
+...
+...
+
+
+4 Letter Words
+--------------
+bare - 4 points
+...
+
+
+5 Letter Words
+--------------
+water - 7 points
+...
+...
+
+
+6 Letter Words
+--------------
+no words
+
+
+7 Letter Words
+--------------
+no words
+
+This program should make use of best practices in code design and modularization. The program should be able to be run from the command line and utilize a main() function that drives the entire program.
+
+Specific functionality should be factored out into dedicated functions and related functions should be collected into modules."""
+
+
+import os
+import scrabble
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+SCRABBLE_WORDS_FILE = CURRENT_DIRECTORY + "/scrabble_list.txt"
+
+
+def main():
+    rack_letters = input("Enter the each letter in your rack: ")
+    is_valid_scrable_rack = scrabble.validate_scrable_rack(rack_letters)
+    if not is_valid_scrable_rack:
+        return
+    all_possible_letter_combos = scrabble.get_all_possible_letter_combos(rack_letters)
+    all_possible_scrable_words = scrabble.load_valid_scrable_words(SCRABBLE_WORDS_FILE)
+    valid_words = scrabble.get_sorted_valid_words_from_letter_combos(all_possible_letter_combos, all_possible_scrable_words)
+    top_15_words_per_length = scrabble.save_top_15_words_for_each_length(valid_words)
+    scrabble.display_top_words_for_each_length(top_15_words_per_length)
+
+
+if __name__ == "__main__":
+    main()
---
+++ assignment_6/scrabble.py
@@ -0,0 +1,90 @@
+import re
+import itertools
+
+TILE_SCORE = {
+    "a": 1,
+    "c": 3,
+    "b": 3,
+    "e": 1,
+    "d": 2,
+    "g": 2,
+    "f": 4,
+    "i": 1,
+    "h": 4,
+    "k": 5,
+    "j": 8,
+    "m": 3,
+    "l": 1,
+    "o": 1,
+    "n": 1,
+    "q": 10,
+    "p": 3,
+    "s": 1,
+    "r": 1,
+    "u": 1,
+    "t": 1,
+    "w": 4,
+    "v": 4,
+    "y": 4,
+    "x": 8,
+    "z": 10,
+}
+LETTER_FORMAT_PATTERN = re.compile(r"^(?:[a-zA-z][\s,]?){1,7}$")
+LETTER_EXTRACTOR_PATTERN = re.compile(r"[a-zA-Z]")
+
+def validate_scrable_rack(rack_letters):
+    if not LETTER_FORMAT_PATTERN.match(rack_letters):
+        print(
+            "Letters must be in valid format with 7 letters max. Examples: trabwqf | t r a b w q f | t,r,a,b,w,q,f"
+        )
+        return False
+    return True
+
+def get_all_possible_letter_combos(rack_letters):
+    rack_letters = LETTER_EXTRACTOR_PATTERN.findall(rack_letters.lower())
+    all_possible_permutations = []
+    for i in range(1, len(rack_letters)):
+        length_of_possible_words = i + 1
+        all_possible_permutations += itertools.permutations(
+            rack_letters, length_of_possible_words
+        )
+    return ["".join(char) for char in all_possible_permutations]
+
+def load_valid_scrable_words(filename):
+    return set(
+        open(filename).read().splitlines()
+    )
+
+def get_sorted_valid_words_from_letter_combos(letter_combos, valid_words_set):
+    validated = []
+    for permutation in letter_combos:
+        if permutation.upper() not in valid_words_set:
+            continue
+        score = 0
+        for char in permutation:
+            score += TILE_SCORE[char]
+        validated.append((len(permutation), score, permutation))
+    return sorted(validated, reverse=True)
+
+def save_top_15_words_for_each_length(words_data_list):
+    word_count = 0
+    save_words_for_length = {}
+    current_length = -1
+    for length, score, word in words_data_list:
+        if length != current_length:
+            current_length = length
+            word_count = 0
+        if word_count == 15:
+            word_count = 0
+            continue
+        word_count += 1
+        result_string = f"{word} - {score} points"
+        save_words_for_length[length] = save_words_for_length.get(length, []) + [result_string]
+    return save_words_for_length
+
+def display_top_words_for_each_length(top_words_per_length):
+    for length, words in top_words_per_length.items():
+        print(f"\n{length} Letter Words\n--------------")
+        words.reverse()
+        for word_score_string in words:
+            print(word_score_string)
