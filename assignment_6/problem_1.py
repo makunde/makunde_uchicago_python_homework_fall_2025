7934b75308bb3b46e97f767752539ecc49dcabb8
George Martin <georgeg10live@gmail.com>
2025-11-18 13:01:38 -0600

    update problem 1 to always save the scrabble file in the same directory as the file. use regex to get the word list

--- assignment_6/problem_1.py
+++ assignment_6/problem_1.py
@@ -3,10 +3,10 @@ Question 3
 
 In Scrabble, the score of a word is the calculated by summing the point value of each individual letter.
 The point value of each letter is assigned based on the frequency of the letters in the game. Common letters
-in English words have a lower score, (eg. c=3, a=1, t=1). Less common letters have a higher point value 
+in English words have a lower score, (eg. c=3, a=1, t=1). Less common letters have a higher point value
 (eg. z=10, q=10, x=8)
 
- 
+
 The following are examples of words and their score:
   `cat` has a point value of 5 where c=3 + a=1 + t=1
   `tax` has a point value of 10, where t=1 + a=1 + x=8
@@ -16,8 +16,8 @@ during game play.  It should be noted that many of the world's best Scrabble pla
 waste of time (and brain power) to memorize the definitions of these words.
 
 Write a program, using best practices in Python, that calculates the score for legal 3 letter words,
-sorts them by score, and writes the words to a file. 
- 
+sorts them by score, and writes the words to a file.
+
 Include the score when writing the words to the file, so you know which words are the most valuable
 to start memorizing. Just like the professional Scrabble players, you do not need to concern yourself
 with learning the definitions, so there is no need to write them to the file.
@@ -38,7 +38,7 @@ When the program runs, it should complete the following tasks:
    If words have the same score, sort the words alphanumerically. For example `car` should be
    listed before `cat` with the same score of 5.
  - Write the words out to a file named `3_letter_words_sorted.txt`. Format your file following this example:
- 
+
      zzz -> 30
      zuz -> 21
      ...
@@ -53,16 +53,23 @@ scoring, sorting, and writing to a file. The module has been started in the repo
 includes a function `points_for_letter()` to return the points for a given letter. Use a `main()` function
 as the driver for your program as written below:
 """
+
 import words
+import sys
+import os
+
 
 def main():
     """Main driver for your program"""
-    scores = words.get_scores_from_file("3_letter_words.txt")
+    args = sys.argv[1:]
+    if len(args) > 1:
+        sys.stderr.write("This program takes in 1 text file max")
+        sys.exit()
+    scores = words.get_scores_from_file(args[0])
     scores = words.sort_scores(scores)
-    words.write_scores_to_file(scores,"3_letter_words_sorted.txt")
-
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    words.write_scores_to_file(scores, current_directory + "/3_letter_words_sorted.txt")
 
 
 if __name__ == "__main__":
     main()
-
--- assignment_6/words.py
+++ assignment_6/words.py
@@ -1,27 +1,53 @@
 """
 words.py Module for Scrabble scoring
 """
+
 import re
+
+WORD_PATTERN = re.compile(r"(?:^|\n)([a-zA-Z]{3})\b")
 
 
 def points_for_letter(letter):
     """Return the points as an integer for a given letter according to Scrabble
     letter frequency tables.
-    """  
-    
+    """
+
     # Dictionary to look up the point value for each letter. In the dictionary,
     # the key is the letter and the value is the point value
-    tile_score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
-                 "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
-                 "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
-                 "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
-                 "x": 8, "z": 10}
+    tile_score = {
+        "a": 1,
+        "c": 3,
+        "b": 3,
+        "e": 1,
+        "d": 2,
+        "g": 2,
+        "f": 4,
+        "i": 1,
+        "h": 4,
+        "k": 5,
+        "j": 8,
+        "m": 3,
+        "l": 1,
+        "o": 1,
+        "n": 1,
+        "q": 10,
+        "p": 3,
+        "s": 1,
+        "r": 1,
+        "u": 1,
+        "t": 1,
+        "w": 4,
+        "v": 4,
+        "y": 4,
+        "x": 8,
+        "z": 10,
+    }
     return tile_score[letter]
+
 
 def get_scores_from_file(filename):
     text = open(filename).read()
-    word_pattern = re.compile(r"(?:^|\n)([a-zA-Z]{3})\b")
-    words_list = word_pattern.findall(text)
+    words_list = WORD_PATTERN.findall(text)
     scores = []
     for word in words_list:
         score = 0
@@ -30,12 +56,13 @@ def get_scores_from_file(filename):
         scores.append((word, score))
     return scores
 
+
 def sort_scores(scores):
     return sorted(scores, key=lambda x: x[1])
+
 
 def write_scores_to_file(scores, filename):
     with open(filename, "w") as f:
         for score in scores:
             score_text = f"{score[0]} -> {score[1]}\n"
             f.write(score_text)
-    
