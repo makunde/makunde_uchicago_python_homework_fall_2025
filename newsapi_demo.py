6646086a464aaa95b434e390b053e0a8b3d86abd
George Martin <georgeg10live@gmail.com>
2025-11-24 14:46:24 -0600

    formating

--- newsapi_demo.py
+++ newsapi_demo.py
@@ -10,14 +10,14 @@ API_KEY = "XXXXXXXXXX"
 url = f"https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=5&apiKey={API_KEY}"
 
 # Search headlines with a query using the `q` parameter
-#url = f"https://newsapi.org/v2/everything?q=scrabble&sortBy=publishedAt&pageSize=5&apiKey=={API_KEY}"
+# url = f"https://newsapi.org/v2/everything?q=scrabble&sortBy=publishedAt&pageSize=5&apiKey=={API_KEY}"
 
 # Request URL Data
 r = requests.get(url)
 
 # Convert into JSON
 json_data = r.json()
-#print(json_data)
+# print(json_data)
 
 # Newsapi query information
 status = json_data["status"]
@@ -25,15 +25,14 @@ total_results = json_data["totalResults"]
 articles = json_data["articles"]
 
 # Caveman debugging
-#print("Status:" + status)
-#print("Total Results:", total_results)
+# print("Status:" + status)
+# print("Total Results:", total_results)
 
-#print(articles[0]["description"])
+# print(articles[0]["description"])
 
 # Note the `pageSize` argument in the API can be used to limit the result that
 # are returned
 for article in articles:
-    print("*",article['title'],article['publishedAt'])
-    print("\t",article['description'])
+    print("*", article["title"], article["publishedAt"])
+    print("\t", article["description"])
     print("-----------------------------------------------\n")
-
---
+++ problem_2.py
@@ -0,0 +1,1 @@
+"""Construct a class to implement the abstract data type Fraction. The Fraction class should have at two attributes, numerator and denominator, that are represented by integers."""
