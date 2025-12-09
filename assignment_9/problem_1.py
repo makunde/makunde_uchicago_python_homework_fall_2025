87d04b1f697c642d5c4fd83b1ed0d5d69e1184f5
George Martin <georgeg10live@gmail.com>
2025-12-08 23:05:02 -0600

    problem 1

---
+++ assignment_9/problem_1.py
@@ -0,0 +1,16 @@
+"""
+What is the time complexity in Big-O notation of the following code snippets?
+You can assume that mathematical operations (eg. **, %, + ) take a constant time.
+
+# Snippet 1: O(n)
+# Iterates through each element once to apply the square operation
+map(lambda x: x ** 2, numbers)
+
+# Snippet 2: O(n)
+# Iterates through each element once to check if it's even
+filter(lambda x: x % 2, numbers)
+
+# Snippet 3: O(n)
+# Iterates through each element once to accumulate the sum
+reduce(lambda x,y: x + y, numbers)
+"""
